ce36cebd7e13fb8c0f0b8fa208c67559dcf6caff
yanniboi <yan@yanniboi.com>
2015-04-23 23:14:50 +0100

    Removed vagrant from aptitude as now installed from package.

--- setup/aptitude.sh
+++ setup/aptitude.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Install aptitude programs
 
-sudo apt-get install -y vim guake vagrant software-properties-common git php5 nfs-kernel-server gimp default-jre default-jdk nodejs npm
+sudo apt-get install -y vim guake software-properties-common git php5 nfs-kernel-server gimp default-jre default-jdk nodejs npm
